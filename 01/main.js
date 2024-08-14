aea4cdb5dad8c5f065e9e62563f919f3fdf0c2b6
adseng <1983902539@qq.com>
2024-08-14 10:47:04 +0800

    puppteer 截图 生成pdf

---
+++ 01/main.js
@@ -0,0 +1,27 @@
+// const puppeteer = require('puppeteer')
+
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+        browser: 'chrome',
+        defaultViewport: {
+            width: 1920,
+            height: 1080,
+        },
+        waitForInitialPage: true,
+    })
+    const page = await browser.newPage()
+    // Set screen size
+
+    await page.goto('www.baidu.com', {
+        waitUntil: 'networkidle2',
+    })
+
+    await page.screenshot({path: path.resolve(__dirname, './out/1.png')})
+    await page.pdf({path: path.resolve(__dirname, './out/1.pdf'), format: 'A4'})
+    await browser.close()
+})()
